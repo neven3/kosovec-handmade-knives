a6600b068c2bcf70f69ace09307408d3c5244cc1
neven3 <61207254+neven3@users.noreply.github.com>
2020-07-30 04:36:10 +0200

    Make menu toggable on smaller screens

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const navbar = document.querySelector('.navbar');
+const navbarMenu = document.querySelector('.navbar-menu');
+const menuToggler = document.querySelector('.menu-toggler');
 
-window.addEventListener('scroll', (event) => {
+// change navbar style on scroll
+window.addEventListener('scroll', () => {
     if (scrollY > 20) navbar.classList.add('sticky');
     else navbar.classList.remove('sticky');
+});
+
+// toggle menu on hamburger click
+menuToggler.addEventListener('click', () => {
+    navbarMenu.classList.toggle('active');
 });
